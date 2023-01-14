eb7bae20075fcb013f6b1a46de2327871d26d847
melvincwng <melvin.ng@outlook.sg>
2023-01-15 07:42:39 +0800

    Refactor PyCat class & Implement both alive & deceased animations

--- src/pycat.py
+++ src/pycat.py
@@ -11,84 +11,202 @@ import random
 
 class PyCat():
     def __init__(self):
-        # Create a Tkinter window by instantantiating a tk.Tk() object, which we will then be inserting under the window attribute
-        self.window = tk.Tk()
+        self.startAliveAnimation()
+    
+    def startAliveAnimation(self):
+        # Mark pet as alive
+        self.isDeceased = False
+        
+        # Create a Tkinter window by instantantiating a tk.Toplevel() object, which we will then be inserting under the window attribute
+        self.window = tk.Toplevel()
         
         # Make window frameless
         self.window.overrideredirect(True)
 
         # Make window appear on top of others (similar concept to z-index: 999/some very high value in CSS --> to determine stack order of elements)
-        self.window.attributes('-topmost', True)
+        self.window.attributes("-topmost", True)
 
         # Create a Label widget as a container that can be used to store our image (it can also store text)
         # 'bd' refers to the border width or size belonging to the Label widget (in px)
-        self.label = tk.Label(self.window, bd=0, bg='white')
+        self.label = tk.Label(self.window, bd=0, bg="white")
 
         # Turn 'white' background color into transparent color instead
-        self.window.wm_attributes('-transparentcolor', 'white')
+        self.window.wm_attributes("-transparentcolor", "white")
 
         # Pass in the gif file (which is a collection of different frames/images). 
         # We then use a list comprehension to generate an array of PhotoImage widgets of our pet cat (each PhotoImage contains a particular frame/image), and
         # Then set the frame index to 0 initially & also set the current_image attribute to the initial frame index
-        self.alive_animation = [tk.PhotoImage(file='alive.gif',format = 'gif -index %i' %(i)) for i in range(114)]
+        self.animation = [tk.PhotoImage(file="final-combined.gif",format = "gif -index %i" %(i)) for i in range(114)]
         self.frame_index = 0
-        self.current_image = self.alive_animation[self.frame_index]
+        self.current_image = self.animation[self.frame_index]
 
         # We need a previous timestamp to check whether can we advance to the next frame/image for our cat.gif
-        # time.time() returns the current time in seconds since the Epoch)\
+        # time.time() returns the current time in seconds since the Epoch)
         self.previous_timestamp = time.time()
 
-        # Generate random X,Y coordinates for your pet cat to spawn at (X: 0 to 1280 - random, Y: 608 - fixed)
+        # Generate random X,Y coordinates for your pet cat to spawn at (X: 1 to 1279 - random, Y: 608 - fixed)
         SCREEN_WIDTH = self.window.winfo_screenwidth()
         SCREEN_HEIGHT = self.window.winfo_screenheight()
         X_AXIS_LEFT_LIMIT = 0
         X_AXIS_RIGHT_LIMIT = 1280
-        RANDOM_X_AXIS_START_POINT = random.randint(X_AXIS_LEFT_LIMIT, X_AXIS_RIGHT_LIMIT)
+        RANDOM_X_AXIS_START_POINT = random.randint(X_AXIS_LEFT_LIMIT + 1, X_AXIS_RIGHT_LIMIT - 1)
         FIXED_Y_AXIS_START_POINT = SCREEN_HEIGHT - 112
         print("Your screen dimensions: ", SCREEN_WIDTH, SCREEN_HEIGHT)
         print("Random X, Y starting points: ", RANDOM_X_AXIS_START_POINT, FIXED_Y_AXIS_START_POINT)
 
         # Save your randomly generated X,Y coordinates to the x_position and y_position attributes
+        # Save the direction of your pet cat to the initial goRightNow = True and initial goLeftNow = False attributes
         self.x_position = int(RANDOM_X_AXIS_START_POINT)
         self.y_position = int(FIXED_Y_AXIS_START_POINT)
+        self.goRightNow = True;
+        self.goLeftNow = False;
 
-        # Set your tkinter window of size 64x64 pixels to be spawned at the X,Y coordinates of (0 to 1280, 608) - random for X, fixed for Y
+        # Set your tkinter window of size 64x64 pixels to be spawned at the X,Y coordinates of (1 to 1279, 608) - random for X, fixed for Y
         self.window.geometry(f"64x64+{RANDOM_X_AXIS_START_POINT}+{FIXED_Y_AXIS_START_POINT}")
 
-        # Add the image to our Label widget. Now out tkinter window will have the image of our pet cat.
+        # Add the image to our Label widget. Now our tkinter window will have the image of our pet cat.
         self.label.configure(image=self.current_image)
 
         # The .pack() method is used to help display the widget in the window.
         self.label.pack()
 
-        # When mainloop() starts, we will run self.updateFrame() after 0ms
-        self.window.after(0, self.updateFrame)
+        # When mainloop() starts, we will run self.updateFramePosition() after 0ms
+        self.window.after(0, self.updateFramePosition)
 
         # Start the mainloop
         self.window.mainloop()
 
-    def updateFrame(self):
-        # Move to the right by one px
-        self.x_position += 1
+    def startDeceasedAnimation(self):
+        # Mark pet as deceased
+        self.isDeceased = True
 
-        # Advance to the next frame/image if 125ms have passed & we loop back to 1 once last frame is reached
+        # Create a Tkinter window by instantantiating a tk.Toplevel() object, which we will then be inserting under the windowTwo attribute
+        self.windowTwo = tk.Toplevel()
+        
+        # Make window frameless
+        self.windowTwo.overrideredirect(True)
+
+        # Make window appear on top of others (similar concept to z-index: 999/some very high value in CSS --> to determine stack order of elements)
+        self.windowTwo.attributes("-topmost", True)
+
+        # Create a Label widget as a container that can be used to store our image (it can also store text)
+        # 'bd' refers to the border width or size belonging to the Label widget (in px)
+        self.label = tk.Label(self.windowTwo, bd=0, bg="white")
+
+        # Turn 'white' background color into transparent color instead
+        self.windowTwo.wm_attributes("-transparentcolor", "white")
+
+        # Pass in the gif file (which is a collection of different frames/images). 
+        # We then use a list comprehension to generate an array of PhotoImage widgets of our pet cat (each PhotoImage contains a particular frame/image), and
+        # Then set the frame index to 0 initially & also set the current_image attribute to the initial frame index
+        self.animation = [tk.PhotoImage(file="final-combined.gif",format = "gif -index %i" %(i)) for i in range(114, 119)]
+        self.frame_index = 0
+        self.current_image = self.animation[self.frame_index]
+
+        # We need a previous timestamp to check whether can we advance to the next frame/image for our cat.gif
+        # time.time() returns the current time in seconds since the Epoch)
+        self.previous_timestamp = time.time()
+
+        # Generate random X,Y coordinates for your pet cat to spawn at (X: 1 to 1279 - random, Y: 608 - fixed)
+        SCREEN_WIDTH = self.windowTwo.winfo_screenwidth()
+        SCREEN_HEIGHT = self.windowTwo.winfo_screenheight()
+        X_AXIS_LEFT_LIMIT = 0
+        X_AXIS_RIGHT_LIMIT = 1280
+        RANDOM_X_AXIS_START_POINT = random.randint(X_AXIS_LEFT_LIMIT + 1, X_AXIS_RIGHT_LIMIT - 1)
+        FIXED_Y_AXIS_START_POINT = SCREEN_HEIGHT - 112
+        print("Your screen dimensions: ", SCREEN_WIDTH, SCREEN_HEIGHT)
+        print("Random X, Y starting points: ", RANDOM_X_AXIS_START_POINT, FIXED_Y_AXIS_START_POINT)
+
+        # Save your randomly generated X,Y coordinates to the x_position and y_position attributes
+        # Save the direction of your pet cat to the initial goRightNow = True and initial goLeftNow = False attributes
+        self.x_position = int(RANDOM_X_AXIS_START_POINT)
+        self.y_position = int(FIXED_Y_AXIS_START_POINT)
+        self.goRightNow = True;
+        self.goLeftNow = False;
+
+        # Set your tkinter window of size 64x64 pixels to be spawned at the X,Y coordinates of (1 to 1279, 608) - random for X, fixed for Y
+        self.windowTwo.geometry(f"64x64+{RANDOM_X_AXIS_START_POINT}+{FIXED_Y_AXIS_START_POINT}")
+
+        # Add the image to our Label widget. Now our tkinter windowTwo will have the image of our pet cat.
+        self.label.configure(image=self.current_image)
+
+        # The .pack() method is used to help display the widget in the window.
+        self.label.pack()
+
+        # When mainloop() starts, we will run self.updateFramePosition() after 0ms
+        self.windowTwo.after(0, self.updateFramePosition)
+
+        # Start the mainloop
+        self.windowTwo.mainloop()
+
+    def reachedLeftEdge(self):
+        return self.x_position <= 0
+
+    def reachedRightEdge(self):
+        return self.x_position >= 1280
+
+    def reachedInBetweenEdges(self):
+        return self.x_position > 0 and self.x_position < 1280
+
+    def generateNextFrame(self):
+        # Advance to the next frame/image if 100ms have passed & we loop back to 1 once last frame is reached
         current_timestamp = time.time()
-        if current_timestamp > self.previous_timestamp + 0.125:
+        if current_timestamp > self.previous_timestamp + 0.100:
             self.previous_timestamp = time.time()
-            self.frame_index = (self.frame_index + 1) % 114
-            self.current_image = self.alive_animation[self.frame_index]
+            # Cat.gif has 119 frames (114 frames healthy animation, 5 frames deceased animation)
+            if (self.isDeceased):
+                self.frame_index = (self.frame_index + 1) % 5
+            else:
+                self.frame_index = (self.frame_index + 1) % 114
+            self.current_image = self.animation[self.frame_index]
 
         # Set your tkinter window of size 64x64 pixels to be spawned at the random coordinates for X, fixed for Y
         self.window.geometry(f"64x64+{self.x_position}+{self.y_position}")
 
-         # Add the image to our label again
+        # Add the image to our label again
         self.label.configure(image=self.current_image)
         
         # The .pack() method is used to help display the widget in the window.
         self.label.pack()
 
-        # Call updateFrame() again after 125ms
-        self.window.after(125, self.updateFrame)
+        # Call updateFramePosition() again after 100ms
+        self.window.after(10, self.updateFramePosition)
+
+        # If not deceased, call switchToDeceasedAnimation() after 20 minutes
+        not self.isDeceased and self.window.after(6000, self.switchToDeceasedAnimation)
+
+    def updateFramePosition(self):
+        reached_left_screen_edge = self.reachedLeftEdge()
+        reached_right_screen_edge = self.reachedRightEdge()
+        reached_in_between_edges = self.reachedInBetweenEdges()
+
+        if (reached_left_screen_edge):
+            # Once you hit the left edge of the screen, move to the right by one px
+            self.goRightNow = True
+            self.goLeftNow = False;
+            self.x_position += 1
+            self.generateNextFrame()
+        elif (reached_right_screen_edge):
+            # Once you hit the right edge of the screen, move to the left by one px
+            self.goLeftNow = True
+            self.goRightNow = False;
+            self.x_position -= 1
+            self.generateNextFrame()
+        elif (reached_in_between_edges and self.goRightNow):
+            # In between the left and right edges of the screen, move in accordance to direction of goRightNow or goLeftNow
+            self.x_position += 1
+            self.generateNextFrame()
+        else:
+            # In between the left and right edges of the screen, move in accordance to direction of goRightNow or goLeftNow
+            self.x_position -= 1
+            self.generateNextFrame()
+    
+    def switchToDeceasedAnimation(self):
+        # Hide previous instance
+        self.window.withdraw()
         
+        # Create new instance & switch to deceased animation
+        self.startDeceasedAnimation()
+
 if __name__ == '__main__':
     PyCat()
